73576c2ce9795908bf08be5ee8007e2b226b321d
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-03-30 17:19:33 +0200

    IT1 done and setup for IT2

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  
+  const price = product.querySelector('.price span').innerText;
 
-  //... your code goes here
+  const quantity = product.querySelector('.quantity input[type=number]').value;
+
+  let subtotal = Number(price*quantity)
+
+  let subtotalItem = product.querySelector('.subtotal span')
+  subtotalItem.innerText = subtotal
+  
+  
+
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
+  
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
   // end of test
